203407d35520d82013f53c49bd6b429d1f1026bc
BekarysKbtu <157203707+BekarysKbtu@users.noreply.github.com>
2024-02-17 12:19:37 +0600

    Add files via upload

---
+++ WebDev/Test.js
@@ -0,0 +1,22 @@
+
+var data = confirm("Are you homo?")
+
+if(data){
+    alert("Woooow slaaaay🫦💅")
+}
+else if (!data){
+    let data2 = confirm("Are you homophobic?😡")
+    if(!data2){
+        alert("You sooo cute 🥰")
+    }
+    else{
+        alert("Go fuck youself bitch, I am gonna fuck you up 😡")
+    }
+
+}
+/*
+for(var i = 1; i<10;i++){
+    alert("just spam ignore it")
+}
+
+ */
